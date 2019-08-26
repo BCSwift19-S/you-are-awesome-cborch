e5130f48b185187d40f4b0c9928d72a7e8e18603
Carter Borchetta <borchett@bc.edu>
2019-08-26 18:43:40 -0400

    init

---
+++ Week1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by Carter Borchetta on 8/26/19.
+//  Copyright © 2019 Carter Borchetta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("🤑viewDidLoad has executed!")
+        
+    }
+
+    @IBAction func sayItButtonPressed(_ sender: UIButton) {
+        print("👌🏾You clicked the say it button")
+        messageLabel.text = "You Are Not Awesome!"
+        
+    }
+    
+}
+
