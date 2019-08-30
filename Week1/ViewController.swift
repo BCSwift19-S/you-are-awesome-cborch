4176865c977264b2d124aaf24cda885bd4f9d9f9
Carter Borchetta <carterborch@me.com>
2019-08-30 11:36:18 -0400

    2.1 Complete. Learning to commit modified versions.

--- Week1/ViewController.swift
+++ Week1/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("🤑viewDidLoad has executed!")
+        print("🤑viewDidLoad has executed!!")
         
     }
 
